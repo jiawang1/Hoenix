796ff790eacf5339c4054df9f0e65494a048df4d
Jay Wang <jay.wang@sap.com>
2016-09-09 14:36:47 +0800

    fix bug of product info path

--- src/common/rootReducer.js
+++ src/common/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import homeReducer from '../features/categoryAttribute/reducer';
 import sampleReducer from '../features/sample/reducer';
-import productInfoReducer from '../features/product-info/reducer';
+import productInfoReducer from '../features/productInfo/reducer';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
