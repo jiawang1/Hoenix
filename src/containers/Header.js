0899a0f0da42f74cded0d1283f207e04c447c8c5
Shan, Flora <277912917@qq.com>
2016-09-12 16:48:32 +0800

    create homepage - developing standard

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import {Row, Col, Input, Button, Badge} from 'antd';
+import {Row, Col, Badge} from 'antd';
 
-const InputGroup = Input.Group;
 
 
 class Header extends Component{
@@ -11,32 +10,27 @@ class Header extends Component{
 	render(){
 		return (
 <div className="test">
-				<Row type="flex" justify="space-around" align="middle">
-					<Col span={4}>
-						<img className="logoImage" src="../image/logo.png" />
-					</Col>
-					<Col span={2}>
-					   <a href="#">首页</a>
-					</Col>
-					<Col span={2}>
-					   <a href="#">帐户设置</a>
-					</Col>
-					<Col span={2}>
-						<Badge count={15}>
-					   	<a href="#" className="notification">消息</a>
-						</Badge>
-					</Col>
-					<Col span={9}>
-					</Col>				
-					<Col span={5}>
-					<InputGroup >
-<Input style={{ width: 263 }} placeholder="搜索"/>
- <div className="ant-input-group-wrap">
- <Button className="search-button" icon="search"/>
- </div>
- </InputGroup>
- </Col>
- </Row></div>
+	<Row type="flex" justify="space-around" align="middle">
+		<Col span={4}>
+			<img className="logoImage" src="../image/logo.png" />
+		</Col>
+		<Col span={2}>
+		   <a href="#">首页</a>
+		</Col>
+		<Col span={2}>
+		   <a href="#">帐户设置</a>
+		</Col>
+		<Col span={2}>
+			<Badge count={15}>
+		   	<a href="#" className="notification">消息</a>
+			</Badge>
+		</Col>
+		<Col span={9}>
+		</Col>				
+		<Col span={5}>
+		</Col>
+	</Row>
+</div>
 		
 		);
 	}
--- src/features/sample/DefaultPage.js
+++ src/features/sample/DefaultPage.js
@@ -2,7 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from './actions';
-import { Button } from 'antd';
+import { Button, Collapse, Row, Col} from 'antd';
+const Panel = Collapse.Panel;
+
+function callback(key) {
+  console.log(key);
+}
 
 class DefaultPage extends Component {
   static propTypes = {
@@ -26,21 +31,68 @@ class DefaultPage extends Component {
 
   render() {
     return (
-      <div className="home-default-page">
-        <h5>Welcome to Rekit project!</h5>
-        <p>
-          This is the sample page of the project. Seeing this page means everything works well now!<br />
-          This project is initialized with one feature named "home" and two test pages. To remove the test pages, run below commands:
-        </p>
-        <ul>
-          <li>npm run rm:page home/test-page-1</li>
-          <li>npm run rm:page home/test-page-2</li>
-        </ul>
-	  <p>To quickly see how Redux works in the project, here is the demo of a simple counter:</p>
-        <div className="demo-count">
-
-        </div>
-        <p>Enjoy!</p>
+      <div className="main-area home-default-page">
+        <h5>Welcome to Hoenix project!</h5>
+        <p>该项目应用了<a href="http://ant.design/" target="blank">Ant Design</a>的前端框架。Ant Design 采用 
+        <a href="https://facebook.github.io/react/" target="blank">React</a> 封装了一套丰富、灵活、实用的基础组件，可以为业务产品提供强有力的设计支持。</p>
+        <p>想了解Ant Design的组件请在<a href="http://ant.design/docs/react/introduce" target="blank">Ant Design</a>页面上方搜索</p>
+        <p>以下是项目前端的开发指南和规范：</p>
+        <Row gutter={20}>
+          <Col span={12}>
+            <h5>js 规范：</h5>
+            <Collapse defaultActiveKey={['1']} onChange={callback}>
+              <Panel header="js文件结构" key="1">
+                <p>每个页面对应src/features下面的一个单独的文件夹。该初始文件夹中包涵：</p>
+                <ul>
+                  <li>actions.js：</li>
+                  <li>constants.js：</li>
+                  <li>index.js：</li>
+                  <li>reducer.js：</li>
+                  <li>route.js：</li>
+                  <li>selectors.js：</li>
+                  <li>XxxPage.js：页面主体</li>
+                  <li>XxxPage.less：页面上独有的css</li>
+                  <li>style.less：定义该页面所有引用的less文件</li>
+                </ul>
+              </Panel>
+              <Panel header="js命名规则" key="2">
+                <p>页面文件夹以小驼峰法命名，例如：分类属性管理 categoryAttribute   商品信息查询  productInfo。</p>
+                <p>页面主体的js文件以大驼峰法命名，以“Page”结尾，例如：分类属性管理 CategoryAttributePage   商品信息查询  ProductInfoPage。</p>
+                <p>页面组件的js文件以大驼峰法命名，gird以“Grid”结尾，例如：ProductGrid.js；dialog以“Dialog”结尾，例如：AttributeDialog。</p>
+              </Panel>
+              <Panel header="js" key="3">
+                <p>第三行</p>
+              </Panel>
+            </Collapse>
+          </Col>
+          <Col span={12}>
+            <h5>css 规范：</h5>
+            <Collapse defaultActiveKey={['1']} onChange={callback}>
+              <Panel header="css文件结构" key="1">
+                <p>项目上css文件是层层引入的结构，可以修改的部分在features/和styles/下面。Ant Design中的所有css文件在node_modules/antd/lib/style下面，请勿修改这些文件，以免产品升级时样式覆盖的麻烦。如需修改组件自带的样式，请另写css覆盖。</p>
+                <p>与features文件夹同级有styles文件夹，该文件夹中包涵：</p>
+                <ul>
+                  <li>index.less：项目中所有引用的less文件，可以通过调整文件引入顺序实现覆盖。</li>
+                  <li>base.less：所有要覆盖原组件样式的css写在这里</li>
+                  <li>common.less</li>
+                  <li>lesshat.less</li>
+                  <li>reset.css</li>
+                  <li>mixins.less：项目中UI通用规范</li>
+                </ul>
+                <p>在每个页面独有的文件夹中包涵了style.less文件，定义了该页面所有引用的less文件，需用“@import './XxxPage.less';”语句将其引入。</p>
+                <p>页面中的图片统一放在src/image下面</p>
+              </Panel>
+              <Panel header="css命名规则" key="2">
+                <p>每个页面独有的less文件与页面主结构js文件命名相同，即XxxPage.less。</p>
+                <p>less文件中类的命名方式以中划线分割单词，一些单词可以简写，例如“hn-prod-desc”即“hoenix-product-description”。</p>
+                <p>全局变量的命名方式与类的命名相同，例如“@link-hover-decoration”。</p>
+              </Panel>
+              <Panel header="css全局变量" key="3">
+                <p>css全局变量主要在mixins.less中定义，需注释变量含义。</p>
+              </Panel>
+            </Collapse>
+          </Col>
+        </Row>
       </div>
     );
   }
