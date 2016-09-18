7e0c5f7988bf2068364d9067e1b858ef7bcdd6ab
Shan, Flora <277912917@qq.com>
2016-09-18 17:56:54 +0800

    tree layout bug fix

--- src/features/stockLevel/DefaultPage.js
+++ src/features/stockLevel/DefaultPage.js
@@ -25,77 +25,81 @@ export class DefaultPage extends Component {
 
     return (
       <div className="stock-level main-area">
-        <Row gutter={20}>
-          <Col span={4} className="attr-tree p-lr-m">          
-            <Tree>
-              <TreeNode title="空调" key="0-0">
-                <TreeNode title="分体空调" key="0-0-0" >
-                  <TreeNode title="挂机" key="0-0-0-0" />
-                  <TreeNode title="柜机" key="0-0-0-1" />
-                  <TreeNode title="吸顶机" key="0-0-0-2" />
-                </TreeNode>
-                <TreeNode title="中央空调" key="0-0-1">
-                  <TreeNode title="家用中央空调" key="0-0-1-0" />
-                  <TreeNode title="商用中央空调" key="0-0-1-1" />
-                  <TreeNode title="辅材" key="0-0-1-2" />
-                  <TreeNode title="新风系统" key="0-0-1-3" />
-                </TreeNode>
-                <TreeNode title="风幕机" key="0-0-2" >
-                </TreeNode>
+        <div className="attr-tree-l">
+          <Tree>
+            <TreeNode title="空调" key="0-0">
+              <TreeNode title="分体空调" key="0-0-0" >
+                <TreeNode title="挂机" key="0-0-0-0" />
+                <TreeNode title="柜机" key="0-0-0-1" />
+                <TreeNode title="吸顶机" key="0-0-0-2" />
               </TreeNode>
-            </Tree>
-          </Col>         
-          <Col span={6}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>
-          </Col>
-          <Col span={6}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>
-          </Col>
-          <Col span={7}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>              
-          </Col>
-        </Row> 
-        <Row gutter={16}>
-          <Col offset={4} span={6}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>
-          </Col>
-          <Col span={6}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>
-          </Col>
-          <Col span={7}>
-            <FormItem
-              label="商品编码"
-              labelCol={{ span: 10 }}
-              wrapperCol={{ span: 14 }}>
-              <Input size="default" />
-            </FormItem>              
-          </Col>
-        </Row>          
+              <TreeNode title="中央空调" key="0-0-1">
+                <TreeNode title="家用中央空调" key="0-0-1-0" />
+                <TreeNode title="商用中央空调" key="0-0-1-1" />
+                <TreeNode title="辅材" key="0-0-1-2" />
+                <TreeNode title="新风系统" key="0-0-1-3" />
+              </TreeNode>
+              <TreeNode title="风幕机" key="0-0-2" >
+              </TreeNode>
+            </TreeNode>
+          </Tree>
+        </div>
+        <div className="search-field-r">
+          <Row>       
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>
+            </Col>
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>
+            </Col>
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>              
+            </Col>
+          </Row> 
+          <Row>
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>
+            </Col>
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>
+            </Col>
+            <Col span={8}>
+              <FormItem
+                label="商品编码"
+                labelCol={{ span: 10 }}
+                wrapperCol={{ span: 14 }}>
+                <Input size="default" />
+              </FormItem>              
+            </Col>
+          </Row>
+        </div>
+        <div className="clr">
+        </div>                
       </div>
     );
   }
