64ca20e6fc067a6d5d590029549ac5f1ee2a715f
Jay Wang <jay.wang@sap.com>
2017-05-02 09:25:23 +0800

    use async

--- src/common/commonAction.js
+++ src/common/commonAction.js
@@ -75,73 +75,9 @@ const __fetch = (_method) => async (option) => {
 	}
 };
 
-
-
-const __fetch1 = (_method) => (option) => {
-
-	let ops = Object.assign({}, {
-		credentials: 'same-origin',
-	}, option, { method: _method });
-
-	if (rootContext) {
-		if (ops.url.charAt(0) === '.') {
-			ops.url = ops.url.slice(1);
-		} else if (ops.url.charAt(0) !== '/') {
-			ops.url = '/' + ops.url;
-		}
-		ops.url = rootContext + ops.url;
-	}
-
-	if (ops.param) {
-		var sParam = Object.keys(ops.param).reduce((pre, current) => {
-			if (getType(ops.param[current]) === 'String' || getType(ops.param[current]) === 'Number'
-				|| getType(ops.param[current]) === 'Boolean') {
-				return pre + (pre.length > 0 ? "&" : '') + current + '=' + encodeURIComponent(ops.param[current]);
-			} else if (getType(ops.param[current]) === 'Array') {
-				return pre + (pre.length > 0 ? "&" : '') + current + '=' + encodeURIComponent(ops.param[current].join(','));
-			} else if (getType(ops.param[current]) === 'Undefined' || getType(ops.param[current]) === 'Null') {
-				return pre;
-			} else if (getType(ops.param[current]) === 'Date') {
-				return pre + (pre.length > 0 ? "&" : '') + current + '=' + ops.param[current].getTime();
-			} else {
-				throw new Error(`param type ${getType(ops.param[current])} for query is not supported`);
-			}
-		}, '');
-
-		if (sParam && sParam.length > 0) {
-			ops.url = ops.url + "?" + sParam
-		}
-	}
-
-	return new Promise((resolve, reject) => {
-		fetch(ops.url, ops).then(response => {
-			if (response.headers.get('__authentication__') === 'failed') {
-				if (window) {
-					response.json().then(json => {
-						window.location.href = json.map.__message__.trim();
-					}).catch(err => {
-						reject(err);
-					});
-				} else {
-					reject('not in browser environment, can not redirect');
-				}
-			} else if (response.headers.get('__authorization__') === 'failed') {
-				if (window) {
-					window.location.href = window.location.href.replace(/^(.*\/#\/)(.*)/, "$1" + NO_AUTH_PATH);
-				}
-				reject("");
-
-			} else {
-				resolve(response);
-			}
-		});
-	});
-
-};
-
 export const get = __fetch('GET');
 export const post = __fetch('POST');
-export const getJson = (option) => {
+export const getJson = async (option) => {
 	if (option.headers) {
 		option.headers['Accept'] = 'application/json';
 	} else {
@@ -261,7 +197,6 @@ const checkStatus = (response) => {
 		return Promise.reject(error);
 	}
 };
-
 
 
 export const getContent = json => json.map.__content__;
